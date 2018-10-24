e1accbfa616243ac28c19f533c62f9b6c2ac2e8b
wangruijie9009 <wangruijie9009-pq@emao.com>
2018-10-24 12:16:42 +0800

    Update application/index/controller/Index.php

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -185,7 +185,7 @@ class Index extends Base
             $new_file = "upload/active/img/".date('Ymd',time())."/";//文件夹位置
             if(!file_exists($new_file)){
                 //检查是否有该文件夹，如果没有就创建，并给予最高权限
-                mkdir($new_file, 0700);
+                mkdir($new_file, 0777, true);//如果创建的多级目录,第三个参数设置为true
             }
             $new_file = $new_file.time().".{$type}";
             if(file_put_contents($new_file, base64_decode(str_replace($result[1], '', $img_base64)))){
