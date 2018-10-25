ae8c4f85164c732ecab79eb8992ae91850cb90d0
wangruijie9009 <wangruijie9009-pq@emao.com>
2018-10-25 16:01:50 +0800

    s

--- application/admin/controller/Order.php
+++ application/admin/controller/Order.php
@@ -201,8 +201,8 @@ class Order extends Base
         //获取到图片的名称
         $urls = pathinfo(urldecode($imgUrl));
         $file_url = parse_url($urls['dirname'])['path'].'/'.$urls['basename'];
-//        var_dump(parse_url($urls['dirname']));
-//        exit;
+        var_dump(PATHINFO_BASENAME);
+        exit;
         $new_name='';
         if(!isset($file_url) || trim($file_url) == ''){
             echo '500';
