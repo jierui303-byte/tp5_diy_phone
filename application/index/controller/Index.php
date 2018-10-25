e5f1ca92b4ab81688d7f4be50a1e446a90546138
wangruijie9009 <wangruijie9009-pq@emao.com>
2018-10-25 15:56:55 +0800

    Update application/index/controller/Index.php

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -217,7 +217,7 @@ class Index extends Base
             }
             $new_file = $new_file.time().".{$type}";
             if(file_put_contents($new_file, base64_decode(str_replace($result[1], '', $img_base64)))){
-                chmod($new_file, 0644);
+                chmod($new_file, 0777);
                 return array(
                     'code' => 1,
                     'msg' => '新文件保存成功',
