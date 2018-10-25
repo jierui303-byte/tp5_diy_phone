e98b42fb94260f978b5c93e06cafd12fde2ed8e0
wangruijie9009 <wangruijie9009-pq@emao.com>
2018-10-25 16:15:07 +0800

    s

--- application/admin/controller/Order.php
+++ application/admin/controller/Order.php
@@ -200,9 +200,9 @@ class Order extends Base
     function downLoadImg($imgUrl){
         //获取到图片的名称
         $urls = pathinfo(urldecode($imgUrl));
-        $file_url = parse_url($urls['dirname'])['path'].'/'.$urls['basename'];
-        var_dump( ROOT_PATH );
-        exit;
+        $file_url = ROOT_PATH.parse_url($urls['dirname'])['path'].'/'.$urls['basename'];
+//        var_dump( ROOT_PATH );
+//        exit;
         $new_name='';
         if(!isset($file_url) || trim($file_url) == ''){
             echo '500';
