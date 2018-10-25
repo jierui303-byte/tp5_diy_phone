cb198bf62158a607f0063256c986ff0881f61699
wangruijie9009 <wangruijie9009-pq@emao.com>
2018-10-25 15:18:12 +0800

    s

--- application/admin/controller/Order.php
+++ application/admin/controller/Order.php
@@ -199,19 +199,20 @@ class Order extends Base
     //文件下载
     public function downLoadImg($imgUrl)
     {
-        var_dump($imgUrl);exit;
-        $down = $_GET['f'];   //获取文件参数
-        $filename = $down.'.zip'; //获取文件名称
-        $dir ="down/";  //相对于网站根目录的下载目录路径
-        $down_host = $_SERVER['HTTP_HOST'].'/'; //当前域名
-
-
-        //判断如果文件存在,则跳转到下载路径
-        if(file_exists(__DIR__.'/'.$dir.$filename)){
-            header('location:http://'.$down_host.$dir.$filename);
-        }else{
-            header('HTTP/1.1 404 Not Found');
-        }
+//        var_dump($imgUrl);exit;
+//        $down = $_GET['f'];   //获取文件参数
+//        $filename = $down.'.zip'; //获取文件名称
+//        $dir ="down/";  //相对于网站根目录的下载目录路径
+//        $down_host = $_SERVER['HTTP_HOST'].'/'; //当前域名
+//
+//
+//        //判断如果文件存在,则跳转到下载路径
+//        if(file_exists(__DIR__.'/'.$dir.$filename)){
+//
+//        }else{
+//            header('HTTP/1.1 404 Not Found');
+//        }
+        header('location:'.$imgUrl);
 
     }
 
