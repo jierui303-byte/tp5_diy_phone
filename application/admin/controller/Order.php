4b14a35345a4e48066a74da4820873b3f8158b4e
wangruijie9009 <wangruijie9009-pq@emao.com>
2018-10-26 12:27:12 +0800

    s

--- application/admin/controller/Order.php
+++ application/admin/controller/Order.php
@@ -28,7 +28,7 @@ class Order extends Base
         $tm = $td+1;
         var_dump($td, $tm);
         $arrSToday = (new \app\common\model\Order())
-            ->where('create_time','between time',[$td, $tm])
+            ->where('create_time','between',[$td, $tm])
             ->select();
 
         $this->assign('data', $arrS);
