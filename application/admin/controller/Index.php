fc0ec6de5957ae2a26274e3b36742bc8addb7cc4
wangruijie9009 <wangruijie9009-pq@emao.com>
2018-10-29 15:24:26 +0800

    s

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,8 +1,10 @@
 <?php
 namespace app\admin\controller;
 
-use app\common\model\User;
-use app\common\model\Index as Indexs;
+use app\common\service\AuthGroup;
+use app\common\service\AuthGroupAccess;
+use app\common\service\Users as UsersService;
+use think\Session;
 use think\Validate;
 
 use think\Controller;
@@ -23,7 +25,20 @@ class Index extends Base
     //查看用户信息
     public function showUserInfo()
     {
+        $uid = Session::get('uid');
 
+        $userInfo = (new UsersService())->getOneByWhere(['uid'=>$uid],'uid,user_name,email,real_name,sex,date_of_birth,address,phone,update_time');
+        //获取角色列表
+        $authGroups = (new AuthGroup())->getAllListsByWhere(
+            [],
+            ['id,title,status']
+        );
+        //获取用户所属角色id
+        $group_id = (new AuthGroupAccess())->getOneByWhere(['uid'=>$uid], ['group_id']);
+
+        $this->assign('authGroups', $authGroups);
+        $this->assign('userInfo', $userInfo);
+        $this->assign('group_id', $group_id['group_id']);
         return $this->fetch('edit-user');
     }
 
